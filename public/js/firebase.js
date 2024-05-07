3a8c69ccbea76ce2b487507adb9dde69ac21596a
shiv <shiv@kali.shivlaptop>
2024-05-07 18:17:48 +0530

    first commit

---
+++ public/js/firebase.js
@@ -0,0 +1,12 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyAEZEJHQU6AnwUbZA1bTSezy-CRssY717Q",
+    authDomain: "blogging-site-a6554.firebaseapp.com",
+    projectId: "blogging-site-a6554",
+    storageBucket: "blogging-site-a6554.appspot.com",
+    messagingSenderId: "738247273737",
+    appId: "1:738247273737:web:ef59b2d9c9d0c60f98b7d8"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
